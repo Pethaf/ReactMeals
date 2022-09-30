a82558419daa351194ff9bac1ceac7bcd1821ba5
Pethaf <pethaf1@gmail.com>
2022-09-30 16:43:41 +0200

    Add basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import {Favorites} from "./components/Favorites";
+import { Meals } from "./components/Meals";
+import { Modal } from "./components/Modal";
+import { Search } from "./components/Search";
 import './App.css';
 
 function App() {
   return (
+    <div> 
+    <Favorites />
+    <Meals /> 
+    <Modal /> 
+    <Search />  
     <h1>React Meals</h1>
-  );
+    </div>
+  ); 
 }
 
 export default App;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,5 @@
+export const Modal = () => {
+    return ( 
+        <p>Modal component</p>
+    )
+}
